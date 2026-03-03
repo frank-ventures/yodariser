693a29ce8fb9fa012bc861110bc4c37fe04f5652
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2026-03-03 15:29:13 +0000

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
+
+const resultP = document.getElementById("result");
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const userInput = event.target.userInput.value;
+  resultP.innerText = yodarise(userInput);
+}
+
+function yodarise(inputText) {
+  const splitInput = inputText.split(" ");
+
+  const twentyPercent = Math.max(Math.ceil(splitInput.length * 0.2), 2);
+
+  const firstPart = splitInput.slice(twentyPercent);
+  const secondPart = splitInput.slice(0, twentyPercent);
+
+  return firstPart.concat(secondPart).join(" ");
+}
